757516a27f9561f3e5218a2632ccf41c23a4c39a
kabweC <kchipopola@aol.com>
2017-12-31 19:51:19 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,9 +1,4 @@
-require_relative '../current_age_for_birth_year.rb'
-
-describe "current_age_for_birth_year method" do
-  it "returns the age of a person based on the year of birth" do
-    age_of_person = current_age_for_birth_year(1984)
-
-    expect(age_of_person).to eq(19)
-  end
-end
+flow_rate = 100
+puts "The flow rate is currently #{flow_rate}."
+flow_rate = 100 / 2
+puts "Now the flow rate is #{flow_rate}!
